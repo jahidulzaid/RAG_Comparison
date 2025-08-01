17e8506c2858c2443dfbeb32ecec777c9ee70c36
Jahidul Islam <jahidulzaid@gmail.com>
2025-08-01 16:06:41 +0600

    updated to fix json format

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from rag.pipeline import run_rag_pipeline
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import JSONLoader
 
