ab37c578245484389e6a9af293fdcdbf4ff96e93
Jahidul Islam <jahidulzaid@gmail.com>
2025-08-01 15:27:02 +0600

    updated langchain ollama

--- rag/pipeline.py
+++ rag/pipeline.py
@@ -1,15 +1,14 @@
 
 from langchain.chains import RetrievalQA
 from retriever.embed_and_index import build_retriever
-from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
 
 def run_rag_pipeline(question: str, file_path: str):
     retriever = build_retriever(file_path)
 
-    llm = Ollama(
-        base_url="http://103.102.42.109:11434",
+    llm = OllamaLLM(
+        base_url="http://localhost:11434",
         model="llama3.1:8b"
-
     )
     qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, chain_type="stuff")
-    return qa.run(question)
+    return qa.invoke({"query": question})
